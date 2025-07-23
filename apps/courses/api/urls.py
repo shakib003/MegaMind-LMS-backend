7d3aefe4b069fd72e02c1a09658b3a44dccd208f
Ahmed Shakib Reza <shakibreza003@gmail.com>
2025-07-23 16:44:59 +0600

    few updates on previous commit message:
    feat(quiz): add models, serializers, and API views for course quizzes
    - Implemented QuizModel with support for MCQ and short-answer types, linked to courses and lessons.
    - Added MCQQuestionModel, MCQOptionModel, and ShortAnswerQuestionModel models for quiz questions.
    - Created serializers for quizzes, MCQ questions/options, and short-answer questions with bulk creation support.
    - Developed API views and endpoints to create, list, retrieve, and delete quizzes under courses.
    - Added docstrings for clarity and documentation.

--- apps/courses/api/urls.py
+++ apps/courses/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . views import (CourseList, CourseDescription,
-                    LessonList, LessonDescription)
+                    LessonList, LessonDescription, CourseQuizList, CourseQuizDescription)
 
 
 # api/v1/course/...
@@ -8,7 +8,7 @@ urlpatterns = [
     path("all/", CourseList.as_view(), name="course-list"),
     path("<int:pk>/", CourseDescription.as_view(), name="course-description"),
     path("<int:pk>/lesson/all/", LessonList.as_view(), name="course_lesson-list"),
-    path("<int:course_id>/lesson/<int:lesson_id>/", LessonDescription.as_view(), name="course_lesson_description")
-    # path("<int:course_id>/mini-quizze/all/", CourseQuizList.as_view(), name="course_quiz_list")
-    # path("<int:course_id>/mini-quizze/a<int:quiz_id>/", CourseQuizDescription.as_view(), name="course_quiz_list")
+    path("<int:course_id>/lesson/<int:lesson_id>/", LessonDescription.as_view(), name="course_lesson_description"),
+    path("<int:course_id>/mini-quizze/all/", CourseQuizList.as_view(), name="course_quiz_list"),
+    path("<int:course_id>/mini-quizze/a<int:quiz_id>/", CourseQuizDescription.as_view(), name="course_quiz_list")
 ]
